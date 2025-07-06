d83b749bf1c6f70d3763cacd8c40d4b26d9b5217
ravihlb <ravihlb@protonmail.com>
2025-07-06 09:08:37 -0300

    fix(+page.ts): sort post list by date desc

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,8 @@
+import type { PostMetadata } from "@/src/shared/types/PostMetadata";
+
 export async function load() {
     const modules = import.meta.glob('./posts/*/metadata.ts');
-    const posts = await Promise.all(
+    const posts: PostMetadata[] = await Promise.all<PostMetadata>(
         Object.entries(modules).map(async ([path, resolver]) => {
             const slug = path.split('/')[2];
             const metadata = await resolver();
@@ -8,6 +10,8 @@ export async function load() {
             return { slug, ...metadata.default };
         })
     );
+
+    posts.sort((a, b) => a.date < b.date ? 1 : -1);
 
     return {
         posts
